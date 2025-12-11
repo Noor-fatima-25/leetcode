60e7bda8ae9ca03ceaea76854b4ff33db1a43f59
Noor-fatima-25 <noorfatimaabidhussain925@gmail.com>
2025-12-10 21:02:26 -0800

    Time: 5 ms (69.33%) | Memory: 46.9 MB (43.40%) - LeetSync

---
+++ 155-min-stack/min-stack.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+class MinStack {
+
+    private Stack<Integer> stack;    // main stack
+    private Stack<Integer> minStack; // stack to track minimums
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        // push to minStack if empty or val <= current min
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        int removed = stack.pop();
+        // if removed element is the current min, pop from minStack
+        if (removed == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
